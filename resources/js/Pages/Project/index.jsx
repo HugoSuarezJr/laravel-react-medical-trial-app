abddf3135d12975c3adfea43b216b205998bbc12
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-07-02 14:41:54 -0400

    add constants file with project and status mapping for classes and text

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -1,5 +1,7 @@
 import Pagination from "@/Components/Pagination";
+import TextInput from "@/Components/TextInput";
 import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
+import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import { Head, Link } from "@inertiajs/react";
 
 export default function Index({ auth, projects }) {
@@ -27,15 +29,41 @@ export default function Index({ auth, projects }) {
                     <th className="px-3 py-3">Actions</th>
                   </tr>
                 </thead>
+
+                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                  <tr className="text-nowrap">
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3">
+                      <TextInput/>
+                    </th>
+                    <th className="px-3 py-3">
+
+                    </th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                    <th className="px-3 py-3"></th>
+                  </tr>
+                </thead>
+
                 <tbody>
                   {projects.data.map((project) => (
-                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                    <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700" key={project.id}>
                       <td className="px-3 py-2">{project.id}</td>
                       <td className="px-3 py-2">
                         <img src={project.image_path} alt={`Image for project ${project.id}`} style={{ width: 60}}/>
                       </td>
                       <td className="px-3 py-2">{project.name}</td>
-                      <td className="px-3 py-2">{project.status}</td>
+                      <td className="px-3 py-2">
+                        <span
+                        className={
+                          "px-2 py-1 rounded text-white " +
+                          PROJECT_STATUS_CLASS_MAP[project.status]
+                        }>
+                          {PROJECT_STATUS_TEXT_MAP[project.status]}
+                        </span>
+                      </td>
                       <td className="px-3 py-2">{project.created_at}</td>
                       <td className="px-3 py-2">{project.due}</td>
                       <td className="px-3 py-2">{project.createdBy.name}</td>
