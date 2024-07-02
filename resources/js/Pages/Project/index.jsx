031a6c8fd868cfc73eb1f9bf8b500dad27f6d1e1
Hugo Suarez <hugo.suarez92@yahoo.com>
2024-07-02 17:32:03 -0400

    create TableHeading component to avoid repeating code

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -5,6 +5,7 @@ import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout";
 import { PROJECT_STATUS_CLASS_MAP, PROJECT_STATUS_TEXT_MAP } from "@/constants.jsx";
 import { Head, Link, router } from "@inertiajs/react";
 import { ChevronUpIcon, ChevronDownIcon } from '@heroicons/react/16/solid'
+import TableHeading from "@/Components/TableHeading";
 
 export default function Index({ auth, projects, queryParams = null }) {
   queryParams = queryParams || {};
@@ -54,16 +55,49 @@ export default function Index({ auth, projects, queryParams = null }) {
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                   <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
                     <tr className="text-nowrap">
-                      <th onClick={ e => sortChanged('id')} className="px-3 py-3 flex items-center justify-between">
-                        ID
-                        <ChevronUpIcon className="w-4"/>
-                        <ChevronDownIcon className="w-4"/>
-                      </th>
+                      <TableHeading
+                        name="id"
+                        sort_field={queryParams.sort_field}
+                        sort_direction={queryParams.sort_direction}
+                        sortChanged={sortChanged}
+                      >ID</TableHeading>
                       <th className="px-3 py-3">Image</th>
-                      <th onClick={ e => sortChanged('name')} className="px-3 py-3">Name</th>
-                      <th onClick={ e => sortChanged('status')} className="px-3 py-3">Status</th>
-                      <th onClick={ e => sortChanged('created_at')} className="px-3 py-3">Create Date</th>
-                      <th onClick={ e => sortChanged('due_date')} className="px-3 py-3">Due Date</th>
+                      <th onClick={ e => sortChanged('name')}>
+                        <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
+                          Name
+                          <div>
+                            <ChevronUpIcon className="w-4"/>
+                            <ChevronDownIcon className="w-4 -mt-2"/>
+                          </div>
+                        </div>
+                      </th>
+                      <th onClick={ e => sortChanged('status')}>
+                        <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
+                          Status
+                          <div>
+                            <ChevronUpIcon className="w-4"/>
+                            <ChevronDownIcon className="w-4 -mt-2"/>
+                          </div>
+                        </div>
+                      </th>
+                      <th onClick={ e => sortChanged('created_at')}>
+                        <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
+                          Create Date
+                          <div>
+                            <ChevronUpIcon className="w-4"/>
+                            <ChevronDownIcon className="w-4 -mt-2"/>
+                          </div>
+                        </div>
+                      </th>
+                      <th onClick={ e => sortChanged('due_date')}>
+                        <div className="px-3 py-3 flex items-center justify-between gap-1 cursor-pointer">
+                          Due Date
+                          <div>
+                            <ChevronUpIcon className="w-4"/>
+                            <ChevronDownIcon className="w-4 -mt-2"/>
+                          </div>
+                        </div>
+                      </th>
                       <th className="px-3 py-3">Created By</th>
                       <th className="px-3 py-3">Actions</th>
                     </tr>
